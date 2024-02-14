6793c2a8488d9cecfde6b1946b0539ae0f0420e8
Ayush-Manware <ayushmanware19@gmail.com>
2024-02-14 12:10:47 +0530

    Data Created

---
+++ index.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        "id":1,
+        "name":"Jaipur",
+        "state":"Rajasthan",
+        "image":" ",
+        "place-one":"Hawa Mahal",
+        "place-one-image":" ",
+        "place-two":"Nahargarh Fort",
+        "place-two-image":" ",
+        "place-three":"City Palace",
+        "place-three-image":" ",
+        "place-four":"Birla Mandir Temple",
+        "place-four-image":" ",
+        "place-five":"Jaigarh Fort",
+        "place-five":""
+    }
+]
